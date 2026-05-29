13ce2f045c7a068099da93a78e6c2ca5338d3c62
Sujal Patil <sujalbpatil21@gmail.com>
2026-05-30 00:37:44 +0530

    Update CorsConfig.java

--- GeoWatch - Backend/src/main/java/com/safety/womensafety/config/CorsConfig.java
+++ GeoWatch - Backend/src/main/java/com/safety/womensafety/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://geo-watch.pages.dev/")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
