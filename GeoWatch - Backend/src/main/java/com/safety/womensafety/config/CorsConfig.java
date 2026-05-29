024e2773fbf44d07b6bd5af13d55a37766db802c
Sujal Patil <sujalbpatil21@gmail.com>
2026-05-30 00:50:20 +0530

    Update CorsConfig.java

--- GeoWatch - Backend/src/main/java/com/safety/womensafety/config/CorsConfig.java
+++ GeoWatch - Backend/src/main/java/com/safety/womensafety/config/CorsConfig.java
@@ -1,3 +1,10 @@
+package com.safety.womensafety.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
